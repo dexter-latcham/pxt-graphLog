a49a57dae149c88f24db67c17cd40b5491611e53
Dexter Latcham <latchamd@lancaster.ac.uk>
2025-03-06 05:53:29 +0000

    lots of updates

--- graph.ts
+++ graph.ts
@@ -5,28 +5,35 @@ enum graphOptions{
 }
 
 namespace graphlog {
-    let chosenGraphType: graphOptions = null;
-    let setGraphTitle: string = "";
-    let headers:string[] = ["foo","bar"];
-    let lastHeaderCheckedIndex=0;
-    let dataLoggerIterator = 0;
+    let graphType: string = "";
+    let graphTitle: string = "";
+    let headers:string[] = [];
     let connected = false;
     let logLiveDatalogger = false;
+    let doSendDataloggerContents = false;
+    let headersRequested = false;
+    let graphTypeRequested = false;
+    let graphTitleRequested = false;
+    let okayToSendData = false;
+    loops.everyInterval(500, function () {
+        if(!connected){
+            return;
+        }
+        if(headersRequested){
+            sendGraphHeaders();
+            headersRequested=false;
+        }
+        if(graphTypeRequested){
+            sendGraphType();
+            graphTypeRequested=false;
+        }
+        if(graphTitleRequested){
+            sendGraphTitle();
+            graphTitleRequested=false;
+        }
+    })
 
-    datalogger.mirrorToSerial(false)
-
-
-    //% block="draw $type graph"
-    export function setGraphType(type: graphOptions) {
-        chosenGraphType = type;
-        sendGraphType();
-    }
-    
-    //% block
-    //% title.defl="My Chart"
-    export function setTitle(title: string) {
-        setGraphTitle = title;
-    }
+    datalogger.mirrorToSerial(false);
     
     export function formatPrintString(data: datalogger.ColumnValue[]):string{
         for(let elem of data){
@@ -54,92 +61,151 @@ namespace graphlog {
     //% blockId=graphAddData
     //% providedData.shadow=lists_create_with
     //% providedData.defl=dataloggercreatecolumnvalue
-    export function addData(providedData:datalogger.ColumnValue[] | (()=>string)){
+    export function addData(providedData:datalogger.ColumnValue[] | (()=>void)){
         if(typeof providedData !== "function"){
             serial.writeLine(formatPrintString(providedData));
             return;
         }
-        //tread providedData as a generator
-        let foo = providedData();
-        while(foo != null){
-            serial.writeLine(foo);
-            foo = providedData();
-        }
-
+        //treat providedData as a function provided to generate data
+        providedData();
     }
     
-    export function getDataloggerContentsFunction():string{
-        if(dataLoggerIterator == datalogger.getNumberOfRows()){
-            return null;
+    export function sendDataloggerContents():void{
+        if(!connected || !okayToSendData){
+            doSendDataloggerContents = true;
+            return;
         }
-        dataLoggerIterator+=1;
-        return datalogger.getRows(dataLoggerIterator-1,1);
+        let dataLoggerSize = datalogger.getNumberOfRows();
+        for(let i=0;i<dataLoggerSize;i++){
+            let line = datalogger.getRows(i,1);
+            serial.writeLine(line);
+        }
     }
     
     //%block="get datalogger contents"
-    export function getDataloggerContents(): () => string{
-        return getDataloggerContentsFunction;
+    export function getDataloggerContents(): () => void{
+        return sendDataloggerContents;
     }
 
+
+    //% block="draw $type graph"
+    export function setGraphType(type: graphOptions) {
+        if(type == graphOptions.BAR){
+            graphType = "bar"
+        }else if(type == graphOptions.PIE){
+            graphType = "pie"
+        }else if(type == graphOptions.LINE){
+            graphType = "line"
+        }
+        graphTypeRequested=true;
+    }
+    
+    //% block
+    //% title.defl="My Chart"
+    export function setTitle(title: string) {
+        graphTitle = title;
+        graphTitleRequested=true;
+    }
 
     //% block="Updata with live logging $on"
     //% blockId=graphUpdateLive
     //% on.shadow=toggleOnOff
     //% on.defl=false
     export function updateLive(on: boolean): void {
-        flashlog.setSerialMirroring(on);
+        if(logLiveDatalogger == false && connected == true){
+            datalogger.mirrorToSerial(on);
+        }
+        logLiveDatalogger = on;
     }
 
     function sendGraphType(){
-        let type = "";
-        if(chosenGraphType == graphOptions.BAR){
-            type = "bar";
-        }else if(chosenGraphType == graphOptions.PIE){
-            type = "pie";
-        }else if(chosenGraphType == graphOptions.LINE){
-            type = "line";
+        if(graphType ==""){
+            return;
         }
-        if(type != ""){
-            serial.writeLine("G:"+type+":G")
+        serial.writeLine("G:"+graphType+":G")
+    }
+
+    function sendGraphTitle(){
+        if(graphTitle != ""){
+            serial.writeLine("T:"+graphTitle+":T");
         }
     }
 
+    function sendGraphHeaders(){
+        if(headers.length == 0){
+            recheckHeaders();
+            if(headers.length == 0){
+                return;
+            }
+        }
+        serial.writeLine("H:"+headers.join(",")+":H");
+    }
+
+    function startSendingData(){
+        if(doSendDataloggerContents){
+            sendDataloggerContents();
+        }else if(logLiveDatalogger){
+            datalogger.mirrorToSerial(true);
+        }
+    }
     function startConnection(){
-
+        if(connected){
+            //need to restart connection
+            datalogger.mirrorToSerial(false);
+        }
+        okayToSendData=false;
+        connected = true;
+        sendGraphTitle();
+        basic.pause(200);
+        sendGraphType();
+        basic.pause(200);
+        recheckHeaders();
+        sendGraphHeaders();
     }
+
     serial.onDataReceived(serial.delimiters(Delimiters.NewLine), function () {
         let input = serial.readString()
         if(input == "C\n"){
-            connected = true;
             startConnection();
+        }else if(input == "O\n"){//okay to start sending data
+            okayToSendData=true;
+            startSendingData();
+        }else if(input == "H\n"){
+            headersRequested=true;
+        }else if(input == "G\n"){
+            graphTypeRequested=true;
+        }else if(input == "T\n"){
+            graphTitleRequested=true;
         }
     });
 
     //datalogger doesn't have a "get headers" so this is a hacky workaround
     //check the first row for headers, if this is wrong then binary search to find the next header insertion line
-    function recheckHeaders(){
-        let lastRowCount = datalogger.getNumberOfRows();
-        if(lastRowCount == 0){
+    function recheckHeaders() {
+        let lastRowIndex = datalogger.getNumberOfRows();
+        if (lastRowIndex == 0) {
             return;
         }
-        let lastRow = datalogger.getRows(lastRowCount-1,1);
+        let lastRow = datalogger.getRows(lastRowIndex - 1, 1);
         let lastRowElems = lastRow.split(",");
         let targetElemCount = lastRowElems.length;
-        if(targetElemCount== headers.length){
+        if (targetElemCount == headers.length) {
             //current headers are correct
             return;
         }
-        for(let i=lastHeaderCheckedIndex;i<lastRowCount;i++){
-            let tmpRow = datalogger.getRows(i,1).split(",");
-            if(tmpRow.length == targetElemCount){
-                // should probably check if tmpRow = current headers 
-                // with some extra entries, if there is no overlap 
-                // then this is likely a malformed string
+        let tmpRow = datalogger.getRows(0, 1).split(",");
+        if(tmpRow.length == targetElemCount){
+            headers = tmpRow;
+            return;
+        }
+        //go backwards from the end of datalogger because it is likely the headers were changed recently
+        for (let i = lastRowIndex - 1; i >= 0; i--) {
+            let tmpRow = datalogger.getRows(i, 1).split(",");
+            if (tmpRow.length < targetElemCount) {
+                tmpRow = datalogger.getRows(i + 1, 1).split(",");
                 headers = tmpRow;
-                lastHeaderCheckedIndex = i;
                 return;
             }
         }
     }
 }
-
--- test.ts
+++ test.ts
@@ -1,46 +1,45 @@
-basic.showIcon(IconNames.Heart)
-graphlog.setTitle("My Chart")
-
-datalogger.deleteLog()
-datalogger.mirrorToSerial(true)
-
-function linePlotTest(){
-    graphlog.setGraphType(graphOptions.LINE)
-    graphlog.setTitle("A graph plotting data")
-    datalogger.includeTimestamp(FlashLogTimeStampFormat.Milliseconds)
-    let noise = 0;
+datalogger.deleteLog(datalogger.DeleteType.Full)
+function testLine(){
+    graphlog.setGraphType(graphOptions.LINE);
+    datalogger.includeTimestamp(FlashLogTimeStampFormat.None)
     let currentVal = Math.random() * 20
     let trend = (Math.random() - 0.5) * 0.5
     let trendPeriod = 20
-    let i=0;
-    while(true){
+    for(let i=0;i<50;i++){
         if (i % trendPeriod == 0) {
             trend = (Math.random() - 0.5) * 0.5
         }
-        noise = (Math.random() - 0.5) * 2
+        let noise = (Math.random() - 0.5) * 2
         currentVal += trend + noise
         currentVal = Math.max(0, Math.min(20, currentVal))
-        let timeStamp = (control.millis() / 1000000);
         let sinVal = Math.sin(i) * 3;
-        datalogger.log(
-            datalogger.createCV("random trend", currentVal),
-            datalogger.createCV("sin wave", sinVal)
-        )
-        basic.pause(200);
-        i+=1;
+        datalogger.log(datalogger.createCV("Time(ms)", control.millis()),
+            datalogger.createCV("x", currentVal),
+            datalogger.createCV("y", sinVal));
     }
-}
-
-function barPlotTest(){
-    graphlog.setGraphType(graphOptions.PIE);
-    datalogger.includeTimestamp(FlashLogTimeStampFormat.None)
-    let i=0;
-    while(true){
-        datalogger.log(
-            datalogger.createCV("item", i)
-        )
-        basic.pause(200);
-        i+=1;
+    let offset = control.millis();
+    for(let i=50;i<100;i++){
+        if (i % trendPeriod == 0) {
+            trend = (Math.random() - 0.5) * 0.5
+        }
+        let noise = (Math.random() - 0.5) * 2
+        currentVal += trend + noise
+        currentVal = Math.max(0, Math.min(20, currentVal))
+        let sinVal = Math.sin(i) * 3;
+        datalogger.log(datalogger.createCV("Time(ms)", control.millis()-offset),
+            datalogger.createCV("x", currentVal),
+            datalogger.createCV("y", sinVal));
     }
+    graphlog.addData(graphlog.getDataloggerContents());
 }
-linePlotTest();
+function testBar(){
+    graphlog.setGraphType(graphOptions.BAR);
+    let owl = randint(2,3);
+    let dove = randint(5,6);
+    for(let i=0;i<5;i++){
+        datalogger.log(datalogger.createCV("owl",owl),
+            datalogger.createCV("dove", dove))
+    }
+    graphlog.addData(graphlog.getDataloggerContents());
+}
+testLine();
